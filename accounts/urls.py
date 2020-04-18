30961fbb20f86086539e0eb9a4ba9c6c4a2b7197
Arman <arman.dnt84@gmail.com>
2020-04-18 19:49:55 +0600

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -18,4 +19,8 @@ urlpatterns = [
     path('update_form/<str:pk>/', views.updateForm , name='update_form'),
     path('delete_order/<str:pk>/', views.deleteForm, name='delete_order'),
     
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uid64>/<token>/', auth_views.PasswordResetCofirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
